9e8acb4198b39a5b5e8dc4879f0aa98cc16c9aa2
Usuario <patty_rosembluth@ubuntu.(none)>
2014-10-27 23:49:29 -0400

    Pagina principal

---
+++ features/steps/bienvenida_steps.rb
@@ -0,0 +1,9 @@
+Given(/^dado que me encuentro en la pagina principal$/) do
+  visit '/'
+end
+
+Then(/^Veo el mensaje "(.*?)" y la imagen "(.*?)"$/) do |mensaje, imagen|
+  last_response.body.should =~ /#{mensaje}/m
+  last_response.should have_xpath("//img[@src=\"/public/images/#{imagen}\"]")
+end
+
