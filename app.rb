b16da9b84c3c61c094a52d6b14613c576390c36a
Usuario <patty_rosembluth@ubuntu.(none)>
2014-11-06 00:22:15 -0400

    Pedir pista

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 
 #llenar diccionario de palabras
+def iniciar_juego
 $words ||= []
 $words << 'imagen'
 $words << 'eucalipto'
@@ -10,25 +11,65 @@ $words << 'casa'
 $total_chances = 5
 $wrong_try = 0
 $right_guess = ''
+char = ''
+$track = ''
+	
+end
 
 get '/' do
-  erb :bienvenida
+	erb :bienvenida
 end
 
 post '/elegir_palabra' do
-$word = $words[rand($words.length) - 1]
-get_placeholder
-erb :juego
+	iniciar_juego
+	$word = $words[rand($words.length) - 1]
+	get_placeholder
+	erb :juego
 end
 
 post '/juego' do
-$letra=params[:letra]
-erb :juego
+	game
+	erb :juego
 end
+
+post '/pedir_pista' do
+	$track = $word[rand($word.length) - 1]
+	params[:letra] = $track
+	$wrong_try += 1	 
+	game
+	erb :juego
+	
+end
+
 
 def get_placeholder
   $placeholder = ''
-  $word.chars { |char| 
-    $placeholder += ($right_guess.include? char)? char : '#'
-  }
+  $word.chars { |char| $placeholder += ($right_guess.include? char)? char : '#'}
+  unless $placeholder.include? '#'
+	 erb :win
+	end
+end
+
+def game ()
+	char = params[:letra]
+	if $word.include? char
+		if($right_guess.include? char)
+	    	#mensaje 'ya se ingreso la letra'
+	      erb :bienvenida
+	    else
+	      $right_guess = $right_guess + char.to_s
+	      get_placeholder
+	      erb :juego
+	    end
+	else
+	  #mensaje de error
+	   $wrong_try += 1	 
+	   if ($wrong_try == $total_chances)
+	    erb :lose      
+	      
+	    else
+	     erb :juego
+	    end
+	end
+	
 end
